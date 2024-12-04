ad6df11f6fa59b4515893bc957fa053dde274123
Apple User <appleuser@Apples-MacBook-Pro.local>
2024-12-04 12:18:36 +0100

    landing done

--- src/app/pages/frontoffice/landing-page/landing-page.component.ts
+++ src/app/pages/frontoffice/landing-page/landing-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { StorageService } from 'src/app/shared/services/storage.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
+  standalone: true,
 })
 export class LandingPageComponent {
+
+  private storageService = inject(StorageService)
+
+  info : any = null
+
+  ngOnInit(): void {
+    this.getInfo();
+  }
+
+  getInfo(){
+
+    this.info = this.storageService.get('User')
+    console.log("info",this.info)
+
+  }
 
 }
--- src/app/pages/frontoffice/pop-up-login/pop-up-login.component.ts
+++ src/app/pages/frontoffice/pop-up-login/pop-up-login.component.ts
@@ -13,6 +13,11 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { HttpProxy } from 'src/app/shared/proxies/http.proxy';
 import { CommonModule } from '@angular/common';
+import { throwError } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+import { AuthService } from 'src/app/shared/services/authentication.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'pop-up-login',
@@ -30,12 +35,13 @@ import { CommonModule } from '@angular/common';
     MatSnackBarModule,
     CommonModule
   ],
-  providers: [AuthProxy,HttpProxy],
+  providers: [AuthService,HttpProxy],
 })
 export class PopUpLoginComponent {
   readonly dialogRef = inject(MatDialogRef<PopUpLoginComponent>);
-  private authProxy = inject(AuthProxy);
+  private authService = inject(AuthService)
   private snackBar = inject(MatSnackBar);
+  private router = inject(Router)
 
   loginData: Login = { email: '', password: '' };
   isLoading = false; // Controla el estado del spinner
@@ -47,11 +53,20 @@ export class PopUpLoginComponent {
   async onLoginClick(): Promise<void> {
     this.isLoading = true; // Muestra el spinner
     try {
-      const result = await this.authProxy.login(this.loginData);
-      this.snackBar.open('Login successful!', 'Close', { duration: 3000 }); // Muestra notificación
-      this.dialogRef.close(result); // Cierra el popup con los datos
+      const info = await this.authService.login(this.loginData);
+    
+      if (info!= undefined){
+        this.snackBar.open('Welcome!!  '+ info.name, 'Close', { duration: 7000 }); // Muestra notificación
+        this.dialogRef.close(info); // Cierra el popup con los datos
+        this.router.navigate(['/landing']); // Redirige automáticamente
+        
+      }else{
+        this.snackBar.open('Login failed. Please try again.', 'Close', { duration: 7000 }); // Notificación de error;
+      }
+       
+      
     } catch (error) {
-      this.snackBar.open('Login failed. Please try again.', 'Close', { duration: 3000 }); // Notificación de error
+      this.snackBar.open('Login failed. Please try again.', 'Close', { duration: 7000 }); // Notificación de error
       console.error('Login failed:', error);
     } finally {
       this.isLoading = false; // Oculta el spinner
